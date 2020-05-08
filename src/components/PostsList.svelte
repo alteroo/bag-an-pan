48056a0a31b6065ef8b82b9e3977a268e8551a1d
Travis CI <build@travis-ci.com>
2020-05-08 06:19:04 -0500

    Updated endpoint for the example

--- src/components/PostsList.svelte
+++ src/components/PostsList.svelte
@@ -3,7 +3,7 @@
   let posts = [];
   onMount(async () => {
     const response = await fetch(
-      "http://localhost:8080/db/grace_chapel_container",
+      "http://localhost:8080/db/site/grace_chapel_container",
       {
         headers: {
           "Content-Type": "application/json",
@@ -13,6 +13,7 @@
     );
     const json = await response.json();
     posts = json.items;
+
     console.log(posts);
   });
 </script>
